d8ab474aab73db479a493520377e1dd38c78e71d
Anna <anyutka901990@mail.ru>
2022-08-07 15:39:46 +0300

    Exampel 010 Metho Array

---
+++ Example010_MethoArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 6, 7, 3, 89, 35, 65, 3, 5 };
+int n = array.Length;
+
+int find = 3;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
